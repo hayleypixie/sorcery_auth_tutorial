2651d13ccc234e016b03cffd237bd444aed350df
Hayley <hayley@pixielabs.io>
2022-03-14 10:16:12 +0000

    add login/logout functionality

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,30 @@
 class UserSessionsController < ApplicationController
+  before_action :set_user, only: %i[ create ]
+
   def new
   end
 
   def create
+    if @user.present?
+      redirect_back_or_to(:users, notice: 'Login successful')
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
   end
 
   def destroy
+    logout
+    redirect_to(:users, notice: 'Logged out!')
+  end
+
+  private
+
+  def set_user
+    @user = login(user_params[:email], user_params[:password])
+  end
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root 'users#index'
+  # User
   resources :users
+  # User Session
+  get '/login', to: 'user_sessions#new', as: :login
+  post '/login', to: 'user_sessions#create'
+  get '/logout', to: 'user_sessions#destroy', as: :logout
 end
